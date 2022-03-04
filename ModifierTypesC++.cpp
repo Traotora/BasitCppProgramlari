acc0ea87ab034c03e7281c48e08ef959e11ac265
Mahir TAŞTEMÜR <70233223+Traotora@users.noreply.github.com>
2022-03-04 18:27:30 +0300

    Add files via upload

---
+++ ModifierTypesC++.cpp
@@ -0,0 +1,34 @@
+#include"iostream"
+#include"conio.h"
+using namespace std;
+int main()
+{
+	 unsigned int isaretsizSayi;
+	 short unsigned int isaretsizShortSayi;
+	 short int shortSayi;	
+	 long int longSayi;
+	 
+	 isaretsizSayi= -4;
+	 isaretsizShortSayi= 5;
+	 shortSayi=50000;
+	 longSayi=40000;
+	 
+	 cout<< "isaretsiz sayi : "<<isaretsizSayi<<endl;
+	 cout<< "isaretsiz short sayi : "<<isaretsizShortSayi<<endl;
+	 cout<<"short sayi : "<<shortSayi<<endl;
+	 cout<<"long sayi : "<<longSayi<<endl;
+	  /// NOT ///
+	 // unsigned int sayilar
+	 // unsigned sayilar // Burada int yazmadýðýnýzda program otomatik olarak int gibi algýlar.
+	 
+	 // volalite: volatile deðiþken tanýmlayýcýsý Eðer bir deðiþken bildiriminin baþýna volatile ifadesini 
+	 //getirirsek, derleyici bu deðiþken deðerinin program içinde herhangi bir zamanda, program içinde yer alan 
+	 //kodlar dýþýnda, farklý kaynaklar tarafýndan deðiþtirilebileceðini anlar.
+	 //
+	 // volalite int sayi8; //volalitenin programa dahil edilme örneði.
+	 //
+	 getch();
+	 return 0;
+	 
+	 // ------------------------------------------------------------ by trao ---
+}
---
+++ defineConstOrnekleri.cpp
@@ -0,0 +1,30 @@
+#include"iostream"
+#include"conio.h"
+#define pi 3.14
+//#include"local.h"
+using namespace std;
+int main()
+{
+/*	
+	char harf;
+	cout<<"Merhaba \n!!!!!!!";
+	*/
+	
+/*	string mesaj="Akademik c++";
+	string mesaj2= "Akademik \
+c++";
+	string mesaj3= "Akademik" " c++";
+	cout<<mesaj<<endl;
+	cout<<mesaj2<<endl;
+	cout<<mesaj3<<endl;
+	*/
+	cout<<"pi sayisi: "<<pi<<endl;
+	//pi =34 // define ile girdiðimiz deðerler sabittir. Bu þekilde deðiþtirilemez.
+	const int yolcusayisi = 45;
+	cout<< yolcusayisi<<endl;
+	yolcusayisi=3;// const ile girdiðimiz deðerler de bu þekilde deðiþtirilemez.
+	getch();
+	return 0;
+	
+	// ------------------------------------------------------------ by trao ---
+}
