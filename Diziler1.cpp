6681d6faae2ecd1c0652eb58a01a674fa5045663
Mahir TAŞTEMÜR <70233223+Traotora@users.noreply.github.com>
2022-03-28 17:55:22 +0300

    Add files via upload

---
+++ Diziler1.cpp
@@ -0,0 +1,24 @@
+#include"iostream"// cout
+#include"cstdlib"//rand,srand
+#include"ctime"
+#include"conio.h"
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL,"Turkish");
+	
+	string a[5];
+	a[0]="sýfýr";
+	a[1]="bir";
+	a[2]="iki";
+	a[3]="üç";
+	a[4]="dört";
+
+	
+	cout<<a[2]<<endl; // Dizinin 3. elemanýný ekrana basýyor. -trao
+	
+	
+	
+	getch();
+	return 0;
+}
