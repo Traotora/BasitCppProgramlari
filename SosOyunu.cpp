bdf802af523d4ef3d1364f090baddf54eaee6a0e
M. TAŞTEMÜR <70233223+Traotora@users.noreply.github.com>
2024-05-31 14:17:41 +0300

    Add files via upload

---
+++ SosOyunu.cpp
@@ -0,0 +1,12 @@
+ #include"iostream"
+ #include"locale.h"
+ 
+ using namespace std;
+ main()
+ {
+ 
+ cout<<"\t[1, 2, 3]\n\t[3, 4, 5]\n[6, 7, 8]\n[0, 3, 6]\n[1, 4, 7]\n[2, 5, 8]\n[0, 4, 8]\n[2, 4, 6]"<<endl;
+    
+    
+ return 0;   
+}
